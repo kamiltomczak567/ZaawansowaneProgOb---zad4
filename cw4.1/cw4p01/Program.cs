856de6bf28530c3ddfe736c9f672ccb4593e41b7
kamiltomczak567 <kamil.tomczak567@googlemail.com>
2017-03-28 19:22:08 +0200

    first commit

---
+++ cw4.1/cw4p01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cw4p01
+{
+
+    public class Uczelnia {
+
+       private DateTime DataRekrutacji;
+       private static Uczelnia _obiekt;
+
+       private Uczelnia()
+       {
+           //this.DataRekrutacji = DataRekrutacji;
+       }
+
+       public static Uczelnia tworzenieObiektu(DateTime DataRekrutacji)
+       {
+           //sprawdzamy czy już utworzyliśmy instancję klasy
+           if (_obiekt == null)
+           {
+               //jeśli nie to ją tworzymy
+               _obiekt = new Uczelnia();
+               _obiekt.DataRekrutacji = DataRekrutacji;
+           }
+           //zwracamy instancję obiektu zapisanego
+           //w stacznym polu naszej klasy
+           return _obiekt;
+       }
+
+       
+
+        public void RekrutujStudentow() {
+            Console.WriteLine("Data następnej rekrutacji to: "+this.DataRekrutacji);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = new DateTime(2017,05,15,7,0,0);
+            DateTime date2 = new DateTime(2017,06,15,8,0,0);
+
+            Uczelnia UWM = Uczelnia.tworzenieObiektu(date1);
+            Uczelnia Stanford = Uczelnia.tworzenieObiektu(date2);
+
+            UWM.RekrutujStudentow();
+            Stanford.RekrutujStudentow();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ cw4.2/cw4p02/cw4p02/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cw4p02
+{
+   public interface ITelefon {
+         void wybierzNumer();
+         void wyslijWiadomosc();
+         void pokazTyp();
+    }
+
+    class ZwyklyTelefon:ITelefon { 
+        
+      public void  wybierzNumer()
+        {
+ 	        Console.WriteLine("Wybierz numer");
+        }
+
+        public void  wyslijWiadomosc()
+        {
+ 	        Console.WriteLine("Wysyłam wiadomosc");
+        }
+
+        public void  pokazTyp()
+        {
+ 	        Console.WriteLine("Pokaz typ");
+        }
+}
+
+    class Dekorator:ITelefon {
+
+        protected ZwyklyTelefon _telefon;
+
+        public Dekorator(ZwyklyTelefon _telefon) {
+            this._telefon = _telefon;
+        }
+
+        public void  wybierzNumer()
+        {
+            _telefon.wybierzNumer();
+        }
+
+        public void  wyslijWiadomosc()
+        {
+            _telefon.wyslijWiadomosc();
+        }
+
+        public void  pokazTyp()
+        {
+            _telefon.pokazTyp();
+        }
+}
+
+    class Smartphone : Dekorator {
+
+        private string rodzajTelefonu;
+
+        public Smartphone(ZwyklyTelefon zwykly, string rodzajTelefonu):base (zwykly) {
+            this.rodzajTelefonu = rodzajTelefonu;
+            this._telefon = zwykly;
+        }
+
+        public void dotknijEkran() {
+            Console.WriteLine("Dotykasz ekran");
+        }
+
+        public void ładujBezprzewodowo() {
+            Console.WriteLine("Ładuje bezprzewodowo");
+        }
+
+    
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ZwyklyTelefon zwyklyTelefon = new ZwyklyTelefon();
+            zwyklyTelefon.pokazTyp();
+            zwyklyTelefon.wybierzNumer();
+            zwyklyTelefon.wyslijWiadomosc();
+
+            Smartphone nowySmartfon = new Smartphone(zwyklyTelefon,"LG");
+            nowySmartfon.ładujBezprzewodowo();
+            nowySmartfon.dotknijEkran();
+
+            
+            Console.ReadKey();
+        }
+    }
+}
